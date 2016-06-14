0a92e32bdff5d0ae5dcbcec09306f91a6e07936d
Luna Versity <mrgodzbrother@gmail.com>
2016-06-14 13:02:59 -0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,69 @@
+<?
+
+// Configuration for Ava's Core by Lunaversity
+// hey a page people can't download *cough* exit core *cough*
+
+//////////////////////
+// Home page config //
+//////////////////////
+
+$server_name = "My Server Name"; // The name that displays on the front-page
+$server_ipaddress = "IP Address Here"; // Your server IP address, (if it's too long it'll be truncated)
+
+////////////////////////
+// About Page config //
+//////////////////////
+
+$about_title = "About Us"; // The header text that you see when you goto the about page
+$about_sub_section = "This is a description about our servers"; // Description that's inside the header background
+
+
+////////////////////////////
+// Donations Page config //
+//////////////////////////
+
+// Names of the packages
+
+$package_1_name = "Package";
+$package_2_name = "Package";
+$package_3_name = "Package";
+
+// Pretty self explanitory
+$package_1_price = '$69';
+$package_2_price = '$69';
+$package_3_price = '$69';
+
+// Package benefits
+
+// First one
+$package_1_benefit_1 = 'Benefit';
+$package_1_benefit_2 = 'Benefit 2';
+$package_1_benefit_3 = 'Benefit 3';
+$package_1_benefit_4 = 'Benefit 4';
+
+// Second one
+$package_2_benefit_1 = 'Benefit';
+$package_2_benefit_2 = 'Benefit 2';
+$package_2_benefit_3 = 'Benefit 3';
+$package_2_benefit_4 = 'Benefit 4';
+
+// Third one
+$package_3_benefit_1 = 'Benefit';
+$package_3_benefit_2 = 'Benefit 2';
+$package_3_benefit_3 = 'Benefit 3';
+$package_3_benefit_4 = 'Benefit 4';
+
+// Fourth one
+$package_4_benefit_1 = 'Benefit';
+$package_4_benefit_2 = 'Benefit 2';
+$package_4_benefit_3 = 'Benefit 3';
+$package_4_benefit_4 = 'Benefit 4';
+
+
+/////////////////////
+// Forum nav link //
+///////////////////
+
+$forums_link = "http://yourwebsite.com/forums"; // Your site then the /forums
+
+?>
